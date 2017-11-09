5da4fd037083da46d2139612f81a6271c9d108fa
GLOBBIMUS <striker1342566@gmail.com>
2017-11-09 10:44:26 -0600

    Implement JavaClient with two threads.
    One thread for reading data from the standard input and sending it to
    the server.
    
    Second thread for reading data from server and writing it to the
    standard input.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,5 +1,6 @@
 package echoserver;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,17 +13,39 @@ public class EchoClient {
 		EchoClient client = new EchoClient();
 		client.start();
 	}
-
 	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
-		}
-		System.out.flush();
+		final Socket socket = new Socket("localhost", PORT_NUMBER);
+		final InputStream socketInputStream = socket.getInputStream();
+		final OutputStream socketOutputStream = socket.getOutputStream();
+		Thread t1 = new Thread(){ //Create first thread for reading from the standard input
+			public void run(){
+				try{
+					int readByte;
+					while ((readByte = System.in.read()) != -1) { //read byte by byte
+						socketOutputStream.write(readByte); //Send it to server
+					}
+					socketOutputStream.flush();
+				  socket.shutdownOutput(); //Shutdown the output once we done reading
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		};
+		t1.start();
+		Thread t2 = new Thread(){ //Create second thread for reading bytes from server
+			public void run(){
+				int socketByte;
+				try {
+					while ((socketByte = socketInputStream.read()) != -1) { //read byte by byte
+						System.out.write(socketByte); //write it to the standard output
+					}
+					System.out.flush();
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		t2.start();
 	}
 }
